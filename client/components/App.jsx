358339c9d70ad92dd7f24c94daedf7238f19f266
jgray1 <john.edwin.gray@gmail.com>
2020-11-03 10:00:52 +1300

    show and hide greeting using state

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component {
+  
+  state = {
+    greeting: "Hello World",
+    showGreeting: true
+  }
+  
+  toggleGreeting = () => {
+    this.setState({showGreeting: !this.state.showGreeting})
+  }
+
+  render() {
+    let lable = this.state.showGreeting ? 'Hide' : 'Show'
+    
+    return (
+    <>
+      <h1>React development has begun!</h1>
+      <button onClick={this.toggleGreeting}>{lable}</button>
+      {this.state.showGreeting && this.state.greeting}
+    </>
+    )
+  }
 }
 
 export default App
