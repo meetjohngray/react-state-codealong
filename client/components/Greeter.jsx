c202a86c06a22c79a6a4adcad77d5db9a8ff9e6e
jgray1 <john.edwin.gray@gmail.com>
2020-11-20 07:07:08 +1300

    convert Greeter from class to functional component

--- client/components/Greeter.jsx
+++ client/components/Greeter.jsx
@@ -1,26 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-class Greeter extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      greeting: props.greeting,
-      showGreeting: true
-    }
-  }
-  
-  toggleGreeting = () => {
-    this.setState({showGreeting: !this.state.showGreeting})
+const Greeter = (props) => {
+   
+  const [greeting, setGreeting] = useState(props.greeting)
+  const [showGreeting, setShowGreeting] = useState(true)
+    
+  const toggleGreeting = () => {
+    setShowGreeting(!showGreeting)
   }
 
-  render() {
-    let lable = this.state.showGreeting ? 'Hide' : 'Show'
-    return (
+  let lable = showGreeting ? 'Hide' : 'Show'
+  return (
     <div>
-      <h2>{this.state.showGreeting && this.state.greeting}</h2>
-      <button onClick={this.toggleGreeting}>{lable}</button>
+      <h2>{showGreeting && greeting}</h2>
+      <button onClick={toggleGreeting}>{lable}</button>
     </div>)
-  }
 }
 
 export default Greeter
